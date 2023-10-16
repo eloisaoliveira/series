e47cdea90ceb62efe24f3337d3e0ba56955d4928
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2023-10-16 12:23:01 -0300

    :sparkles: feat: Tabela de Series

--- src/pages/Series/index.tsx
+++ src/pages/Series/index.tsx
@@ -1,9 +1,32 @@
 import { Header } from "../../components/Header";
+import { SeriesContainer, SeriesTable } from "./styles";
 
 export function Series() {
   return (
     <div>
       <Header />
+
+      <SeriesContainer>
+        <SeriesTable>
+          <tbody>
+            <tr>
+              <td width="50%">Supernatural</td>
+              <td>Data lançamento</td>
+              <td>15/09/2005</td>
+            </tr>
+            <tr>
+              <td width="50%">Stranger Things</td>
+              <td>Data lançamento</td>
+              <td>15/07/2016</td>
+            </tr>
+            <tr>
+              <td width="50%">The Blacklist</td>
+              <td>Data lançamento</td>
+              <td>23/09/2013</td>
+            </tr>
+          </tbody>
+        </SeriesTable>
+      </SeriesContainer>
     </div>
   )
 }
---
+++ src/pages/Series/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const SeriesContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const SeriesTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  /* margin-top: 1.5rem; */
+  td {
+    padding: 1.25rem 2rem;
+    background: ${props => props.theme["gray-700"]};
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
